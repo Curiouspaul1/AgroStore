04ba0c608d942068a92d5370d6231314b82db444
Curious <170408025@live.unilag.edu.ng>
2019-12-31 03:23:08 +0100

    removed unneeded files

--- app/main/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-main = Blueprint('main',__name__,static_folder='',template_folder='templates')
-
-from . import views,errors
-
--- app/main/views.py
+++
@@ -1,7 +0,0 @@
-from . import main
-from flask import render_template,request,session,url_for,redirect
-
-@main.route('/')
-@main.route('/index')
-def index():
-   return render_template('index.html')
