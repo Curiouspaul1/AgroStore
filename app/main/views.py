dddcaff1ef47a449c03122ec666f1a9e41548a49
Curious <170408025@live.unilag.edu.ng>
2020-01-01 02:49:19 +0100

    fixed BUGS!

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,7 @@ from flask import render_template,request,session,url_for,redirect
 @main.route('/index')
 def index():
    return render_template('index.html')
+
+@main.route('/sign_up',methods=['GET','POST'])
+def sign_up():
+	return render_template('sign_in.html')
